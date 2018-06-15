34b36f421583bdc78125c791ebc6cf41cd72cb04
randysuen1991 <randysuen1991@gmail.com>
2018-06-15 08:26:19 +0800

    new

--- Q-Learning.py
+++ Q-Learning.py
@@ -1,3 +1,37 @@
-
 import numpy as np
-import gym
+
+
+
+class OrderBookEnv():
+    
+    _actions = {
+        'hold': np.array([1, 0, 0]),
+        'buy': np.array([0, 1, 0]),
+        'sell': np.array([0, 0, 1])
+    }
+
+    _positions = {
+        'flat': np.array([1, 0, 0]),
+        'long': np.array([0, 1, 0]),
+        'short': np.array([0, 0, 1])
+    }
+    
+    def __init__(self):
+        pass
+    
+    
+    def step(self,action):
+        """Take an action (buy/sell/hold) and computes the immediate reward.
+        Args:
+            action (numpy.array): Action to be taken, one-hot encoded.
+        Returns:
+            tuple:
+                - observation (numpy.array): Agent's observation of the current environment.
+                - reward (float) : Amount of reward returned after previous action.
+                - done (bool): Whether the episode has ended, in which case further step() calls will return undefined results.
+                - info (dict): Contains auxiliary diagnostic information (helpful for debugging, and sometimes learning).
+        """
+        
+        
+        
+        return observation, reward, done, info
