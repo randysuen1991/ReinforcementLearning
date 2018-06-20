a9f36253f7584a7b72624ffb710bba70a0e37667
randysuen1991 <randysuen1991@gmail.com>
2018-06-20 11:21:58 +0800

    new

--- ReinforcementLearningModel.py
+++ ReinforcementLearningModel.py
@@ -84,30 +84,39 @@ class QLearning(ReinforcementLearningModel):
             return action
     
 class Sarsa(ReinforcementLearningModel):
-    def __init__(self,states,actions,env,episodes_size,decay_rate=0.1,learning_rate=0.01,epsilon=0.05):
-        super().__init__(states,actions,env,episodes_size,decay_rate,learning_rate,epsilon)
+    def __init__(self,states,actions,env,episodes_size,decay_rate=0.1,learning_rate=0.01,epsilon=0.05,epsilon_increment=None):
+        super().__init__(states,actions,env,episodes_size,decay_rate,learning_rate,epsilon,epsilon_increment)
     
 class DeepQLearning(ReinforcementLearningModel):
     def __init__(self,states,actions,env,episodes_size,
-                 features_size, memory_size, batch_size,replace_target_size=300,
-                 decay_rate=0.1,learning_rate=0.01,epsilon=0.05,default=True):
-        super().__init__(states,actions,env,episodes_size,decay_rate,learning_rate,epsilon)
+                 features_size, memory_size, batch_size,replace_target_size=300,learn_size=150,
+                 decay_rate=0.1,learning_rate=0.01,epsilon=0.05,epsilon_increment=None,default=True):
+        super().__init__(states,actions,env,episodes_size,decay_rate,learning_rate,epsilon,epsilon_increment)
         self.replace_target_size = replace_target_size
         self.memory_size = memory_size
+        self.learn_size = learn_size
         # the batch_size means how many data we would take from the memory.
         self.batch_size = batch_size
         self.memory = np.zeros((self.memory_size, 2 * self.features_size + 2))
         # A deep Q model gets its own session.
         self.sess = tf.Session()
         self.cost_history = list()
-        self.learn_step_counter = 0
+        self.epsilon_max = 1 - self.epsilon
+        self.epsilon_increment = epsilon_increment
+        self.epsilon = 0 if epsilon_increment is not None else self.epsilon_max
+        
         
         
         if default :
             self._Construct_DefaultModels()
+            self.sess.run(tf.global_variables_initializer())
             
-        # Define replace_target_op here.
+            # If NOT using the default model, the following codes should be handled still.
+            # Define replace_target_op here.
+            target_params = self.targ_model.layers
+            eval_params = self.eval_model.layers
         
+            self.replace_target = [tf.assign(t, e) for t, e in zip(target_params, eval_params)]
             
         
     def _Construct_DefaultModels(self):
@@ -115,7 +124,7 @@ class DeepQLearning(ReinforcementLearningModel):
         self.eval_model = NNM.NeuralNetworkModel()
         self.eval_model.Build(NNU.NeuronLayer(hidden_dim=20))
         self.eval_model.Build(NNU.NeuronLayer(hidden_dim=15))
-        self.eval_model.Build(NNU.NeuronLayer(hidden_dim=10))
+        self.eval_model.Build(NNU.NeuronLayer(hidden_dim=self.actions_size))
         
         # target model and eval model share the same structure.
         self.targ_model = cp.deepcopy(self.eval_model)
@@ -126,15 +135,33 @@ class DeepQLearning(ReinforcementLearningModel):
         state = self.env.Reset()
         
         while True :
+            
             action = self.Predict(state)
-            state_, reward, done = self.env.Step(action)
-            if (step > 200) and (step % 5 == 0) :
-                pass
-            state = state_
+            new_state, reward, done, _ = self.env.Step(action)
+            self._Store_Transition(state,action,reward,new_state)
+            
+            if (step > self.learn_size) and (step % 5 == 0) :
+                self._Learn()
+                
+            state = new_state
             step += 1
             
-    def Predict(self, action, epsilon=None) :
-        pass
+            if step % self.replace_target_iter == 0:
+                self.sess.run(self.replace_target_op)
+                print('\ntarget_params_replaced\n')
+            
+    def Predict(self, state, epsilon=None) :
+        action = self.eval_model.Predict(X_test=state)
+        
+        try :
+            if np.random.uniform(0,1) < 1 - epsilon + epsilon / len(self.actions) :
+                return action
+            else :
+                actions_list = copy.copy(self.actions)
+                actions_list.remove(action)
+                return random.choice(actions_list)
+        except :
+            return action
     
     def _Store_Transition(self,state,action,reward,new_state):
         if not hasattr(self,'memory_counter'):
@@ -144,19 +171,15 @@ class DeepQLearning(ReinforcementLearningModel):
         self.memory[index,:] = transition
         self.memory_counter += 1
         
-    def _Replace_Target_Params(self):
-        pass
+    
     
     def _Learn(self):
-        
-        if self.learn_step_counter % self.replace_target_iter == 0:
-            self.sess.run(self.replace_target_op)
-            print('\ntarget_params_replaced\n')
         
         if self.memory_counter > self.memory_size :
             sample_index = np.random.choice(self.memory_size,size=self.batch_size)
         else :
             sample_index = np.random.choice(self.memory_counter,size=self.batch_size)
+            
         batch_memory = self.memory[sample_index,:]
         
         self.eval_model.Compile(X_train_shape=batch_memory[:,:self.n_features].shape,optimizer=tf.train.RMSPropOptimizer,loss_fun=NNL.NeuralNetworkLoss.MeanSqaured)
@@ -168,7 +191,9 @@ class DeepQLearning(ReinforcementLearningModel):
         q_target = q_eval.copy()
         
         batch_index = np.arange(self.batch_size, dtype=np.int32)
+        # pick the actions done in those steps.
         eval_act_index = batch_memory[:, self.n_features].astype(int)
+        # get the reward if taking that action.
         reward = batch_memory[:, self.n_features + 1]
         
         q_target[batch_index, eval_act_index] = reward + self.gamma * np.max(q_next, axis=1)
@@ -179,3 +204,7 @@ class DeepQLearning(ReinforcementLearningModel):
         
         
         self.cost_history.append(self.cost)
+        
+        
+        self.epsilon = self.epsilon + self.epsilon_increment if self.epsilon < self.epsilon_max else self.epsilon_max
+        self.learn_step_counter += 1
