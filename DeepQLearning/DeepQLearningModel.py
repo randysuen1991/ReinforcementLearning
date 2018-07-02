6001d7089b687683d44078bcc9d2bdc15f4e4d67
孫以恒-新金融商品部-策略交易處 <randysuen@masterlink.com.tw>
2018-07-02 15:48:44 +0800

    new

--- DeepQLearning/DeepQLearningModel.py
+++ DeepQLearning/DeepQLearningModel.py
@@ -3,8 +3,8 @@ import random
 import copy
 import sys
 
-if 'C:\\Users\\randysuen\\Neural-Network' or  'C:\\Users\\ASUS\\Dropbox\\pycode\\mine\\Neural-Network' not in sys.path :
-    sys.path.append('C:\\Users\\randysuen\\Neural-Network')
+if 'C:\\Users\\randysuen\\pycodes\\Neural-Network' or  'C:\\Users\\ASUS\\Dropbox\\pycode\\mine\\Neural-Network' not in sys.path :
+    sys.path.append('C:\\Users\\randysuen\\pycodes\\Neural-Network')
     sys.path.append('C:\\Users\\ASUS\\Dropbox\\pycode\\mine\\Neural-Network')
 
 import tensorflow as tf
@@ -15,8 +15,8 @@ import NeuralNetworkLoss as NNL
 import copy as cp
 import warnings
 
-if 'C:\\Users\\randysuen\\Reinforcement-Learning' or 'C:\\Users\\ASUS\\Dropbox\\pycode\\mine\\Reinforcement-Learning' not in sys.path :
-    sys.path.append('C:\\Users\\randysuen\\Reinforcement-Learning')
+if 'C:\\Users\\randysuen\\pycodes\\Reinforcement-Learning' or 'C:\\Users\\ASUS\\Dropbox\\pycode\\mine\\Reinforcement-Learning' not in sys.path :
+    sys.path.append('C:\\Users\\randysuen\\pycodes\\Reinforcement-Learning')
     sys.path.append('C:\\Users\\ASUS\\Dropbox\\pycode\\mine\\Reinforcement-Learning')
 
 import ReinforcementLearningModel as RLM
@@ -31,7 +31,7 @@ class DeepQLearning(RLM.ReinforcementLearningModel):
         
         
         # Sometimes, the episodes size is determined by the  
-        if hasattr(env,'episodes') :
+        if hasattr(env, 'episodes'):
             if len(env.episodes) < episodes_size :
                 warnings.warn('The available episodes size is less than the desired episodes size, so set the \
                              episodes size to be the available one.')
@@ -61,12 +61,9 @@ class DeepQLearning(RLM.ReinforcementLearningModel):
         self.cost_history = list()
         self.epsilon = epsilon
         self.gamma = gamma
-        
-        
         assert self.memory_size > self.batch_size
-        
-        
-        if default :
+
+        if default:
             self._Construct_DefaultModels()
             self.eval_model.Compile(X_train_shape=(self.batch_size,self.env.features_size),optimizer=tf.train.GradientDescentOptimizer(learning_rate=self.learning_rate),loss_fun=NNL.NeuralNetworkLoss.MeanSqaured)
             self.targ_model.Compile(X_train_shape=(self.batch_size,self.env.features_size),loss_and_optimize=False)
@@ -75,8 +72,7 @@ class DeepQLearning(RLM.ReinforcementLearningModel):
             
             # If NOT using the default model, the following codes should be handled still.
             # Define replace_target_op here.
-            
-            
+
             eval_params = self._Get_parameters(self.eval_model)
             targ_params = self._Get_parameters(self.targ_model)
             
@@ -84,10 +80,10 @@ class DeepQLearning(RLM.ReinforcementLearningModel):
             self.replace_target = [tf.assign(t, e) for t, e in zip(targ_params, eval_params)]
             
         # Should write codes to 
-        else :
+        else:
             pass
             
-    def _Get_parameters(self,model):
+    def _Get_parameters(self, model):
         parameters_list = list()
         for layer in model.layers :
             parameters_dict = layer.parameters
@@ -95,10 +91,7 @@ class DeepQLearning(RLM.ReinforcementLearningModel):
                 parameters_list.append(parameters)
                 
         return parameters_list 
-    
-    
 
-        
     def _Construct_DefaultModels(self):
         
         self.eval_model = NNM.NeuralNetworkModel()
@@ -112,113 +105,94 @@ class DeepQLearning(RLM.ReinforcementLearningModel):
         self.targ_model.Build(NNU.NeuronLayer(hidden_dim=15,transfer_fun=tf.nn.relu))
         self.targ_model.Build(NNU.NeuronLayer(hidden_dim=self.actions_size,transfer_fun=None))
     
-    def Fit(self,plot_cost=False):
-        
-        for i in range(self.episodes_size) :
-            
-            
-            
-            
+    def Fit(self, plot_cost=False):
+        for i in range(self.episodes_size):
             step = 0
             state = self.env.Reset(iteration=i)
             
-            while True :
-                action = self.Predict(state,self.epsilon)
+            while True:
+                action = self.Predict(state, self.epsilon)
                 self.env.actions.append(action)
                 new_state, reward, done = self.env.Step()
-                self._Store_Transition(state.ravel(),action,reward,new_state.ravel())
-                print(state.ravel(),action,reward,new_state.ravel())
-                if done :
-                    action = self.Predict(new_state,self.epsilon)
+                self._Store_Transition(state.ravel(), action, reward, new_state.ravel())
+                print(state.ravel(), action, reward, new_state.ravel())
+                if done:
+                    action = self.Predict(new_state, self.epsilon)
                     self.env.actions.append(action)
                     break
                 
-                if (step > self.learn_size) and (step % 10 == 0) :
+                if (step > self.learn_size) and (step % 10 == 0):
                     self._Learn()
-            
-                
-                
+
                 state = new_state
                 step += 1
             
                 if step % self.replace_target_size == 0:
                     self.sess.run(self.replace_target)
                     print('\ntarget_params_replaced\n')
-                
-        
-        if plot_cost :
+
+        if plot_cost:
             plt.plot(self.cost_history) 
         
         print('Training over!')
                 
     # If an epsilon is passed, it would be greedy strategy, and vice versa.
-    def Predict(self, state, epsilon=None) :
+    def Predict(self, state, epsilon=None):
         
         actions = self.eval_model.Predict(X_test=state)
         action = self.env.DealAction(np.argmax(actions))
         
-        try :
-            
-            if np.random.uniform(0,1) < (1 - epsilon + epsilon / len(self.env.actions_space)) :
+        try:
+            if np.random.uniform(0, 1) < (1 - epsilon + epsilon / len(self.env.actions_space)):
                 return action
-            else :
-                
+            else:
                 actions_list = copy.copy(self.env.actions_space)
                 actions_list.remove(action)
                 return random.choice(actions_list)
         except :
             return action
     
-    def _Store_Transition(self,state,action,reward,new_state):
-        if not hasattr(self,'memory_counter'):
+    def _Store_Transition(self, state, action, reward, new_state):
+        if not hasattr(self, 'memory_counter'):
             self.memory_counter = 0
-        transition = np.hstack((state,action,reward,new_state))
+        transition = np.hstack((state, action, reward, new_state))
         index = self.memory_counter % self.memory_size
-        self.memory[index,:] = transition
+        self.memory[index, :] = transition
         self.memory_counter += 1
-        
-    
-    
+
     def _Learn(self):
         
-        if self.memory_counter > self.memory_size :
-            sample_index = np.random.choice(self.memory_size,size=self.batch_size)
-        else :
-            sample_index = np.random.choice(self.memory_counter,size=self.batch_size)
+        if self.memory_counter > self.memory_size:
+            sample_index = np.random.choice(self.memory_size, size=self.batch_size)
+        else:
+            sample_index = np.random.choice(self.memory_counter, size=self.batch_size)
             
-        batch_memory = self.memory[sample_index,:]
-        
-        
-        
-        q_next, q_eval = self.sess.run([self.targ_model.output, self.eval_model.output],feed_dict={self.targ_model.input:batch_memory[:,-self.env.features_size:],
-                                       self.eval_model.input:batch_memory[:,:self.env.features_size]})
+        batch_memory = self.memory[sample_index, :]
+
+        q_next, q_eval = self.sess.run([self.targ_model.output, self.eval_model.output],
+                                       feed_dict={self.targ_model.input: batch_memory[:, -self.env.features_size:],
+                                                  self.eval_model.input: batch_memory[:, :self.env.features_size]})
         
         q_target = q_eval.copy()
-        
         batch_index = np.arange(self.batch_size, dtype=np.int32)
         # pick the actions done in those steps.
         eval_act_index = batch_memory[:, self.env.features_size].astype(int)
         # get the reward if taking that action.
         reward = batch_memory[:, self.env.features_size + 1]
-        
         q_target[batch_index, eval_act_index] = reward + self.gamma * np.max(q_next, axis=1)
-        
-        _, self.cost = self.sess.run([self.eval_model.train, self.eval_model.loss],
-                                     feed_dict={self.eval_model.input: batch_memory[:, :self.env.features_size],
-                                                self.eval_model.target: q_target})
-        
-        self.cost_history.append(self.cost)
+
+        _, cost = self.sess.run([self.eval_model.train, self.eval_model.loss],
+                                feed_dict={self.eval_model.input: batch_memory[:, :self.env.features_size],
+                                           self.eval_model.target: q_target})
+        self.cost_history.append(cost)
     
     def BackTest(self, states):
         actions = list()
-        for state in states :
-            action = self.Predict(state)    
+        for state in states:
+            action = self.Predict(state)
             actions.append(action)
-            
         return actions
-        
-        
-        
+
 class DoubleDeepQLearning(DeepQLearning):
     def __init__(self, env, memory_size=50, batch_size=40, episodes_size=10,
                  replace_target_size=100,learn_size=30, gamma=0.8,
